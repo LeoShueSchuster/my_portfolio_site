aaa27398b028bf1b197ffa73668b0668d3121684
LeoShueSchuster <leo@careerplug.com>
2019-03-19 09:15:04 -0500

    Fix Landing Page Bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resource :portfolio
   resources :users
   resource :session
   resources :projects
   resources :contacts, only: [:new, :create]
   root "portfolio#index"
+  get "home" => "portfolio#index"
   patch "projects/:id" => "projects#update"
   get   "projects/new" => "projects#new"
 end
