d1aca2662369fead501846b9c2b517473777fcc3
LeoShueSchuster <leo@careerplug.com>
2019-03-20 12:20:55 -0500

    Second try at seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,20 @@
 
 Project.create!([
     { 
-        name: 'The Latinx Mental Health Podcast', 
-        description: 'The Latinx Mental Health Podcast is where we talk to therapists, researchers, artists, activists, and students about their experiences in the intersections of mental health and Latinx identity. In each interview we aim to connect through our voices, our struggles, and our triumphs as we sample a different herbal tea just like abuela used to make.',
+        name: 'The Latinx Mental Health Podcast',
+        description: 
+        %{
+            The Latinx Mental Health Podcast is where we talk to therapists, researchers, artists, activists, and students about their experiences in the intersections of mental health and Latinx identity. In each interview we aim to connect through our voices, our struggles, and our triumphs as we sample a different herbal tea just like abuela used to make.
+        }.squish,
         link: 'https://www.latinxmhpodcast.com/',
         credit: 'Producer, Audio Engineer' 
     },
     { 
-        name: 'Enlaces Latinos', 
-        description: 'I perform with the Hernandez family band with my electric bass guitar. We play music from throughout Latin America.',
+        name: 'Enlaces Latinos',
+        description:
+        %{
+            I perform with the Hernandez family band with my electric bass guitar. We play music from throughout Latin America.
+        }.squish,
         link: 'https://www.facebook.com/Enlaces-Latinos-155778007772062/',
         credit: 'Bass Guitarist' 
     }
