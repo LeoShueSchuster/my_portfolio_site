69b48072b149d0bd988d97e63f50bb0175ae74a3
LeoShueSchuster <leo@careerplug.com>
2019-03-20 12:15:06 -0500

    Attempt adding AdminUser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create!([
+    { 
+        name: 'The Latinx Mental Health Podcast', 
+        description: 'The Latinx Mental Health Podcast is where we talk to therapists, researchers, artists, activists, and students about their experiences in the intersections of mental health and Latinx identity. In each interview we aim to connect through our voices, our struggles, and our triumphs as we sample a different herbal tea just like abuela used to make.',
+        link: 'https://www.latinxmhpodcast.com/',
+        credit: 'Producer, Audio Engineer' 
+    },
+    { 
+        name: 'Enlaces Latinos', 
+        description: 'I perform with the Hernandez family band with my electric bass guitar. We play music from throughout Latin America.',
+        link: 'https://www.facebook.com/Enlaces-Latinos-155778007772062/',
+        credit: 'Bass Guitarist' 
+    }
+])
+
+User.create!([
+    {
+        name: 'Leo (Admin)',
+        password: 'temp',
+        password_confirmation: 'temp',
+        email: 'leoshueschuster@gmail.com',
+        admin: true
+    }
+])
