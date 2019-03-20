4e54e27742d2d7e7cd8e50b584a9c297c7ad54a0
LeoShueSchuster <leo@careerplug.com>
2019-03-20 12:58:21 -0500

    Hope seeding works

--- db/migrate/20190318170155_create_projects.rb
+++ db/migrate/20190318170155_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
       t.string :name
-      t.string :text
+      t.string :description
 
       t.timestamps
     end
