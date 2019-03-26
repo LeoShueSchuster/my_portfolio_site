f2617b553641105278fbd90f493f27dd486ac4d2
LeoShueSchuster <leo@careerplug.com>
2019-03-26 14:56:18 -0500

    Added testing and content 3

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :projects
   resources :contacts, only: [:new, :create]
   root "home#index"
+  get "home" => "home#index"
   patch "projects/:id" => "projects#update"
   get   "projects/new" => "projects#new"
 end
