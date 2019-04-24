e73ca4b6bcee18090c140e414d5d7a8225d96c5a
LeoShueSchuster <leo@careerplug.com>
2019-04-24 15:36:56 -0500

    Contact form repairs part 1

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,11 +6,11 @@ class Contact < MailForm::Base
   attribute :message,     validate: true
   attribute :nickname,    captcha: true
 
-  # def headers
-  #   {
-  #     subject: 'Contact Form',
-  #     to: 'leoshueschuster@gmail.com',
-  #     from: %("#{name}" <#{email}>)
-  #   }
-  # end
+  def headers
+    {
+      subject: 'Contact Form',
+      to: 'leoshueschuster@gmail.com',
+      from: %("#{name}" <#{email}>)
+    }
+  end
 end
